3726dda08209547cf2b078d2e6e6a7372c2660d8
ryanvogel <ryanvogel@192.168.1.117>
2019-08-31 21:33:36 -0400

    new

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,6 +9,99 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		// Arrays for items and their prices
+		int count = scan.nextInt();
+		String[] items = new String[count];
+		double[] itemAmount = new double[count];
+		for (int i = 0; i < count; i++) {
+			items[i] = scan.next();
+			itemAmount[i] = scan.nextDouble();
+		}
 		
+		
+		// Array for counting the people
+		int count2 = scan.nextInt();
+		String[] firstNames = new String[count2];
+		String[] lastNames = new String[count2];
+		double[] totalSpent = new double[count2];
+		
+		for (int i = 0; i < count2; i++) {
+			firstNames[i] = scan.next();
+			lastNames[i] = scan.next();
+			int totalDifferentItems = scan.nextInt();
+			
+			String[] itemsPurchased = new String[totalDifferentItems];
+			int[] amountOfEachItem = new int[totalDifferentItems];
+			for (int x = 0; x < totalDifferentItems; x++) {
+				amountOfEachItem[x] = scan.nextInt();
+				itemsPurchased[x] = scan.next();
+			}
+			
+			for (int x = 0; x < totalDifferentItems; x++) {
+				for (int y = 0; y < count; y++) {
+					if (itemsPurchased[x] == items[y]) {
+						totalSpent[x] += itemAmount[y] * amountOfEachItem[x];
+					}
+				}
+				
+			}
+	
 	}
+	for (int i = 0; i < totalSpent.length; i++) {
+		totalSpent[i] = scan.nextDouble();
+	}
+	scan.close();
+	double sum = calculateValueSum(totalSpent);
+	double min = findValueMin(totalSpent);
+	double max = findValueMax(totalSpent);
+	
+	System.out.println("Biggest: " + min);
+	System.out.println("Smallest: " + max);
+	System.out.println("Average: " + String.format("%.2f", ((double) sum) / ((double) count)));
+	
+	
+	}
+	static double calculateValueSum(double[] vals) {
+		
+		int sum = 0;
+		
+		for (int i=0; i<vals.length; i++) {
+			sum += vals[i];
+		}
+		
+		return sum;
+	}
+	static double findValueMin(double[] vals) {
+		
+		// Initialize current minimum to first value in array.
+		double cur_min = vals[0];
+		
+		// Starting with second value (if any), compare each value
+		// in array with current minimum and replace if smaller.
+		
+		for (int i=1; i < vals.length; i++) {
+			if (vals[i] < cur_min) {
+				cur_min = vals[i];
+			}
+		}
+		
+		return cur_min;
+	}
+	static double findValueMax(double[] vals) {
+		
+		// Initialize current minimum to first value in array.
+		double cur_max = vals[0];
+		
+		// Starting with second value (if any), compare each value
+		// in array with current minimum and replace if smaller.
+		
+		for (int i=1; i < vals.length; i++) {
+			if (vals[i] > cur_max) {
+				cur_max = vals[i];
+			}
+		}
+		
+		return cur_max;
+	}
+
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,33 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		// variable that counts which customer we are looking at 
+		int countCustomers = scan.nextInt();
+			
+		// Arrays to keep track of the customers' first initial, last name, and total cost
+		
+		char[] firstInitial = new char[countCustomers];
+		String[] lastName = new String[countCustomers];
+		double[] totalCost = new double[countCustomers];
+			
+		for (int i = 0; i < countCustomers; i++) {
+			String tmp = scan.next();
+			firstInitial[i] = tmp.charAt(0);
+			lastName[i] = scan.next();
+			int totalItems = scan.nextInt();
+		
+			double sum = 0;
+			for (int j = 0; j < totalItems; j++) {
+				int amount = scan.nextInt();
+				
+				double price = scan.nextDouble();
+				sum = (amount * price) + sum;
+			}
+			totalCost[i] = sum;
+			System.out.println(firstInitial[i] + ". " + lastName[i] + ": " + String.format("%.2f", totalCost[i]));
+		}
+		
 		
 	}
+	
 }
